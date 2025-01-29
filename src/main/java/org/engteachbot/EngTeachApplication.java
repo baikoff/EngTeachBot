abbf9fd3d572c160a85663c7bab1113fe9e46cb9
Artem <baykovartem1994@gmail.com>
2025-01-29 10:20:45 +0300

    1 коммит (пробный)

---
+++ src/main/java/org/engteachbot/EngTeachApplication.java
@@ -0,0 +1,11 @@
+package org.engteachbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngTeachApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EngTeachApplication.class, args);
+    }
+}
